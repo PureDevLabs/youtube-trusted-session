cb70b6fb23630e494cefd1d9f1d79a41c9d74c5c
RustyNail <bergheimer@outlook.com>
2024-08-04 14:55:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# YouTube trusted session Generator
+
+### Install bun on your Linux Server
+
+Open Terminal
+
+```bash
+curl -fsSL https://bun.sh/install | bash
+```
+
+make bun Global executable
+
+```bash
+sudo mv ~/.bun/bin/bun /usr/local/bin/
+```
+
+```bash
+sudo chmod a+x /usr/local/bin/bun
+```
+
+> [!IMPORTANT]  
+> ***close the Terminal after bun installation***
+
+
+### Install Chromium Browser
+```bash
+sudo apt install chromium-browser
+```
+
+---
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run start
+```
+
+This project was created using `bun init` in bun v1.1.3. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
---
+++ index.js
@@ -0,0 +1,55 @@
+import puppeteer from "puppeteer-extra";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+
+const delay = async (time) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+};
+
+puppeteer.use(StealthPlugin());
+
+const options = {
+  headless: true,
+  devtools: true,
+  executablePath: "/usr/bin/chromium-browser",
+  args: ["--no-sandbox", "--disable-setuid-sandbox"],
+};
+
+const browser = await puppeteer.launch(options);
+await browser.pages();
+// console.log(await browser.version());
+const page = await browser.newPage();
+page.setDefaultNavigationTimeout(0);
+
+const url = "https://www.youtube.com/embed/JHjVmEikWLw";
+page.bringToFront();
+
+const client = await page.createCDPSession();
+await client.send("Network.enable");
+
+client.on("Network.requestWillBeSent", (params) => {
+  if (params.request.url.includes("/youtubei/v1/player")) {
+    const data = JSON.parse(params.request.postData);
+    const outout = {
+      visitorData: data.context.client.visitorData,
+      poToken: data.serviceIntegrityDimensions.poToken,
+    };
+    console.log(outout);
+  }
+});
+
+await page.goto(url, { waitUntil: "domcontentloaded" });
+
+await delay(3000);
+await page.evaluate((_) => window.stop());
+
+const playButton = await page.$("#movie_player");
+await playButton.click();
+await delay(1000);
+await playButton.click();
+
+await page.setViewport({ width: 1080, height: 1024 });
+
+// await page.screenshot({ path: "yt.png" });
+await browser.close();
