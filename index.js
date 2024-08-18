edee47d79f560e04f4ed38febef0f4ca49e0bc37
RustyNail <65692019+rustynail1984@users.noreply.github.com>
2024-08-18 06:53:52 +0200

    more stability in shell mode

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ puppeteer.use(StealthPlugin());
 const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36';
 
 const options = {
-  headless: true,
+  headless: 'shell',
   devtools: true,
   executablePath: "/usr/bin/chromium",
   args: ["--no-sandbox", "--disable-setuid-sandbox"],
